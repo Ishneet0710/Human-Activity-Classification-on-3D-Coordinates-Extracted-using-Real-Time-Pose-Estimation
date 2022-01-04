c805ed2c28c92b4081070e464fa754c5d8c230c8
ishneet <ishneetsingh0710@gmail.com>
2022-01-04 16:25:38 +0800

    add files via upload

---
+++ Iris/lda_model_for_iris.py
@@ -0,0 +1,24 @@
+# evaluate a lda model on the dataset
+import numpy as np
+import pandas as pd
+from numpy import mean
+from numpy import std
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+# define dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+dataset = pd.read_csv(url, names=names)
+X = dataset.iloc[:, 0:4].values
+y = dataset.iloc[:, 4].values
+# define model
+model = LinearDiscriminantAnalysis()
+# define model evaluation method
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+# evaluate model
+scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+# summarize result
+print('Mean Accuracy: %.3f (%.3f)' % (mean(scores), std(scores)))
